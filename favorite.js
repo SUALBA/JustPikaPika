31bba7d3bb093024fa4cd7fcc940e83be34904be
sualsa <su.alba@hotmail.com>
2023-07-14 07:10:43 +0200

    pagina dinamica recetas favoritas, se crean, guardan, borran, muestran datos extras raton, mejora CSS

---
+++ favorite.js
@@ -0,0 +1,147 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const favoritesContainer = document.getElementById('favorites-container');
+  const deleteAllButton = document.getElementById('delete-all-button');
+
+  // Obtener las recetas favoritas desde el almacenamiento local
+  const favoriteRecipes = localStorage.getItem('favoriteRecipes');
+  const parsedFavorites = favoriteRecipes ? JSON.parse(favoriteRecipes) : [];
+
+  // Crear las tarjetas interactivas para cada receta favorita
+  parsedFavorites.forEach(recipe => {
+    const card = createCard(recipe);
+    favoritesContainer.appendChild(card);
+  });
+
+  // Asignar el controlador de eventos al botón de eliminar todas las recetas favoritas
+  deleteAllButton.addEventListener('click', removeFavoritesConfirmation);
+
+
+  function createCard(recipe) {
+    const card = document.createElement('div');
+    card.className = 'card';
+    card.dataset.id = recipe.id; // Asignar el ID de la receta utilizando el atributo "dataset"
+
+    // Asignar los detalles de la receta al atributo "title"
+    card.title = `Health Score: ${recipe.healthScore}\nDiets: ${recipe.diets.join(', ')}`;
+
+    card.innerHTML = `
+      <img src="${recipe.image}" alt="${recipe.title}" />
+      <div class="card-content">
+        <h3>${recipe.title}</h3>
+        <p>${recipe.description}</p>
+        <button class="remove-button">Eliminar</button>
+        <button class="remove-definitely-button" style="display: none;">Eliminar Definitivamente</button>
+      </div>
+    `;
+
+    // Hacer la tarjeta interactiva y arrastrable
+    makeInteractive(card);
+
+    // Agregar controladores de eventos a los botones
+    const removeButton = card.querySelector('.remove-button');
+    const removeDefinitelyButton = card.querySelector('.remove-definitely-button');
+    removeButton.addEventListener('click', function(event) {
+      removeFavoriteConfirmation(event, recipe.id);
+    });
+    removeDefinitelyButton.addEventListener('click', function(event) {
+      removeFavoriteDefinitely(event, recipe.id);
+    });
+
+    return card;
+  }
+
+  // Función para hacer las tarjetas interactivas y arrastrables
+  function makeInteractive(element) {
+    let isDragging = false;
+    let initialX, initialY;
+    let offsetX = 0, offsetY = 0;
+
+    element.addEventListener('mousedown', startDrag);
+    element.addEventListener('mousemove', drag);
+    element.addEventListener('mouseup', endDrag);
+
+    element.addEventListener('touchstart', startDrag);
+    element.addEventListener('touchmove', drag);
+    element.addEventListener('touchend', endDrag);
+
+    function startDrag(e) {
+      if (e.type === 'mousedown') {
+        initialX = e.clientX;
+        initialY = e.clientY;
+      } else if (e.type === 'touchstart') {
+        initialX = e.touches[0].clientX;
+        initialY = e.touches[0].clientY;
+      }
+
+      isDragging = true;
+      offsetX = element.offsetLeft;
+      offsetY = element.offsetTop;
+    }
+
+    function drag(e) {
+      if (!isDragging) return;
+
+      e.preventDefault();
+
+      let currentX, currentY;
+      if (e.type === 'mousemove') {
+        currentX = e.clientX;
+        currentY = e.clientY;
+      } else if (e.type === 'touchmove') {
+        currentX = e.touches[0].clientX;
+        currentY = e.touches[0].clientY;
+      }
+
+      const deltaX = currentX - initialX;
+      const deltaY = currentY - initialY;
+
+      element.style.left = `${offsetX + deltaX}px`;
+      element.style.top = `${offsetY + deltaY}px`;
+    }
+
+    function endDrag() {
+      isDragging = false;
+    }
+  }
+
+  // Función para mostrar una confirmación antes de eliminar la receta favorita
+  function removeFavoriteConfirmation(event, recipeId) {
+    event.stopPropagation(); // Detener la propagación del evento para evitar que se active el evento de eliminar la receta completa
+
+    const confirmDelete = confirm('¿Estás seguro de que deseas eliminar esta receta favorita?');
+
+    if (confirmDelete) {
+      const cardContent = event.target.closest('.card-content');
+      const removeButton = cardContent.querySelector('.remove-button');
+      const removeDefinitelyButton = cardContent.querySelector('.remove-definitely-button');
+
+      removeButton.style.display = 'none';
+      removeDefinitelyButton.style.display = 'block';
+    }
+  }
+   // Función para mostrar una confirmación antes de eliminar todas las recetas favoritas
+   function removeFavoritesConfirmation() {
+    const confirmDelete = confirm('¿Estás seguro de que deseas eliminar todas las recetas favoritas?');
+
+    if (confirmDelete) {
+      favoritesContainer.innerHTML = ''; // Eliminar todas las tarjetas de recetas favoritas
+      localStorage.removeItem('favoriteRecipes'); // Eliminar las recetas del almacenamiento local
+    }
+  }
+
+  // Función para eliminar definitivamente una receta favorita
+  function removeFavoriteDefinitely(event, recipeId) {
+    event.stopPropagation(); // Detener la propagación del evento para evitar que se active el evento de la tarjeta completa
+
+    // Eliminar la receta del almacenamiento local
+    const favoriteRecipes = localStorage.getItem('favoriteRecipes');
+    const parsedFavorites = favoriteRecipes ? JSON.parse(favoriteRecipes) : [];
+    const updatedFavorites = parsedFavorites.filter(recipe => recipe.id !== recipeId);
+    localStorage.setItem('favoriteRecipes', JSON.stringify(updatedFavorites));
+
+    // Eliminar la tarjeta de la receta de la vista
+    const card = event.target.closest('.card');
+    card.remove();
+  }
+});
+
--- recipe.js
+++ recipe.js
@@ -1,44 +1,61 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const recipeId = urlParams.get('id');
-  
-    if (recipeId) {
-      fetchRecipeDetails(recipeId);
-    }
-  
-    function fetchRecipeDetails(id) {
-      const recipeDetailsEndpoint = `https://api.spoonacular.com/recipes/${id}/information?apiKey=aac38ef2db854c74b8b9f59ac48acbf6&number=6&query=`;
-  
-      fetch(recipeDetailsEndpoint)
-        .then(response => response.json())
-        .then(recipe => {
-          displayRecipeDetails(recipe);
-        })
-        .catch(error => {
-          console.log("Error al obtener los detalles de la receta:", error);
-        });
-    }
-  
-    function displayRecipeDetails(recipe) {
-      const recipeTitle = document.getElementById('recipe-title');
-      const ingredientList = document.getElementById('ingredient-list');
-      const instructionsList = document.getElementById('instructions-list');
-  
-      recipeTitle.textContent = recipe.title;
-  
-      recipe.extendedIngredients.forEach(ingredient => {
-        const li = document.createElement('li');
-        li.textContent = `${ingredient.original}`;
-  
-        ingredientList.appendChild(li);
+  const urlParams = new URLSearchParams(window.location.search);
+  const recipeId = urlParams.get('id');
+
+  if (recipeId) {
+    fetchRecipeDetails(recipeId);
+  }
+
+  function fetchRecipeDetails(id) {
+    const recipeDetailsEndpoint = `https://api.spoonacular.com/recipes/${id}/information?apiKey=aac38ef2db854c74b8b9f59ac48acbf6&number=6&query=`;
+
+    fetch(recipeDetailsEndpoint)
+      .then(response => response.json())
+      .then(recipe => {
+        displayRecipeDetails(recipe);
+      })
+      .catch(error => {
+        console.log("Error al obtener los detalles de la receta:", error);
       });
-  
-      recipe.analyzedInstructions[0].steps.forEach(step => {
-        const li = document.createElement('li');
-        li.textContent = step.step;
-  
-        instructionsList.appendChild(li);
-      });
-    }
-  });
-  
+  }
+
+  function displayRecipeDetails(recipe) {
+    const recipeTitle = document.getElementById('recipe-title');
+    const ingredientList = document.getElementById('ingredient-list');
+    const instructionsList = document.getElementById('instructions-list');
+
+    recipeTitle.textContent = recipe.title;
+
+    recipe.extendedIngredients.forEach(ingredient => {
+      const li = document.createElement('li');
+      li.textContent = `${ingredient.original}`;
+
+      ingredientList.appendChild(li);
+    });
+
+    recipe.analyzedInstructions[0].steps.forEach(step => {
+      const li = document.createElement('li');
+      li.textContent = step.step;
+
+      instructionsList.appendChild(li);
+    });
+
+    const favoriteButton = document.createElement('button');
+    favoriteButton.innerHTML = '<img src="images/corePink.png" class="heart-icon">';
+    favoriteButton.addEventListener('click', function() {
+      addToFavorites(recipe);
+    });
+
+    document.body.appendChild(favoriteButton);
+  }
+
+  function addToFavorites(recipe) {
+    const favoriteRecipes = localStorage.getItem('favoriteRecipes');
+    const parsedFavorites = favoriteRecipes ? JSON.parse(favoriteRecipes) : [];
+
+    parsedFavorites.push(recipe);
+    localStorage.setItem('favoriteRecipes', JSON.stringify(parsedFavorites));
+
+    window.location.href = 'favorite.html';
+  }
+});
